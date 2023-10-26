c86261e7c73eb153e5aea8051403cea83c82de13
PMT <pmt9819@gmail.com>
2023-10-26 20:51:32 +0700

    Add OpenGraph content

--- components/NextHead.js
+++ components/NextHead.js
@@ -4,9 +4,9 @@ export default function NextHead({ title, description, children }) {
   return (
     <Head>
       <meta charSet="UTF-8" />
-      {children}
       <meta name="description" content={description || ''} />
       <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
+      {children}
       <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <title>{title}</title>
