6b42c5c407c10d03489003f1bfe9740716015a48
PMT <pmt9819@gmail.com>
2023-11-05 16:05:01 +0700

    Update assets

--- pages/fighter/[id].js
+++ pages/fighter/[id].js
@@ -316,104 +316,106 @@ export default function Page({ fighter }) {
                       </div>
                     </div>
                   </Tab>
-                  <Tab key="aid" title="Viện trợ">
-                    <div className="sm:rounded-lg sm:border sm:border-slate-400/25 sm:p-4">
-                      <div className="space-y-3">
-                        <h3 className="font-semibold">Kỹ năng viện trợ</h3>
-                        <div className="flex cursor-pointer items-center gap-2 rounded-br-lg rounded-tl-lg border border-orange-400/25 p-2 drop-shadow hover:bg-orange-400/25">
-                          <Image
-                            src={`/assets/items/skill_hero${(fighter.id - 100000)
-                              .toString()
-                              .padStart(3, '0')}_a2.png`}
-                            alt=""
-                            width={100}
-                            height={100}
-                            className="h-12 w-12 rounded-lg border border-slate-400/25 sm:h-14 sm:w-14"
-                          />
-                          <div className="flex-1 space-y-1">
-                            <div className="font-medium text-slate-200">
-                              Sóng Phượng hoàng Caesar
+                  {fighter.aid && (
+                    <Tab key="aid" title="Viện trợ">
+                      <div className="sm:rounded-lg sm:border sm:border-slate-400/25 sm:p-4">
+                        <div className="space-y-3">
+                          <h3 className="font-semibold">Kỹ năng viện trợ</h3>
+                          <div className="flex cursor-pointer items-center gap-2 rounded-br-lg rounded-tl-lg border border-orange-400/25 p-2 drop-shadow hover:bg-orange-400/25">
+                            <Image
+                              src={`/assets/items/skill_hero${(fighter.id - 100000)
+                                .toString()
+                                .padStart(3, '0')}_a2.png`}
+                              alt=""
+                              width={100}
+                              height={100}
+                              className="h-12 w-12 rounded-lg border border-slate-400/25 sm:h-14 sm:w-14"
+                            />
+                            <div className="flex-1 space-y-1">
+                              <div className="font-medium text-slate-200">
+                                Sóng Phượng hoàng Caesar
+                              </div>
+                              <div className="text-sm">
+                                Bắt đầu từ vòng đầu tiên, võ sĩ linh hồn rùa sẽ thực hiện một pha
+                                nguy hiểm hoặc nó sẽ kích hoạt sau khi thực hiện tổng cộng 2 pha
+                                nguy hiểm trong vòng này.
+                              </div>
                             </div>
-                            <div className="text-sm">
-                              Bắt đầu từ vòng đầu tiên, võ sĩ linh hồn rùa sẽ thực hiện một pha nguy
-                              hiểm hoặc nó sẽ kích hoạt sau khi thực hiện tổng cộng 2 pha nguy hiểm
-                              trong vòng này.
+                          </div>
+                        </div>
+                        <Divider className="my-4" />
+                        <div className="space-y-3">
+                          <h3 className="font-semibold">Đẳng viện trợ</h3>
+                          <div className="flex cursor-pointer flex-col gap-2 rounded-br-lg rounded-tl-lg border border-orange-400/25 px-2 py-4 drop-shadow">
+                            <div className="flex items-center gap-4">
+                              <div className="w-20 rounded-md bg-white py-0.5 text-center text-sm text-black">
+                                Trắng
+                              </div>
+                              <div className="flex-1">Kích hoạt kỹ năng viện trợ</div>
+                            </div>
+                            <Divider className="my-2" />
+                            <div className="flex items-center gap-4">
+                              <div className="w-20 rounded-md bg-emerald-500 py-0.5 text-center text-sm text-white">
+                                Lục
+                              </div>
+                              <div className="flex-1">
+                                Sau khi vào trận, bạn sẽ tăng vĩnh viễn tỷ lệ sát thương của bản
+                                thân thêm 15%, tỷ lệ tránh sát thương thêm 30% và tỷ lệ chặn đòn
+                                thêm 10%, khe kỹ năng số 1 được mở.
+                              </div>
+                            </div>
+                            <Divider className="my-2" />
+                            <div className="flex items-center gap-4">
+                              <div className="w-20 rounded-md bg-blue-500 py-0.5 text-center text-sm text-white">
+                                Lam
+                              </div>
+                              <div className="flex-1">
+                                Thêm 150% sát thương phòng thủ của chính nó, kỹ năng 2 được kích
+                                hoạt, +1 số lần sử dụng
+                              </div>
+                            </div>
+                            <Divider className="my-2" />
+                            <div className="flex items-center gap-4">
+                              <div className="w-20 rounded-md bg-purple-500 py-0.5 text-center text-sm text-white">
+                                Tím
+                              </div>
+                              <div className="flex-1">
+                                Phục hồi 12% máu tối đa cho người chiến đấu có lượng máu thấp nhất
+                                và thêm máu tương đương 200% khả năng phòng thủ của chính mình; Kỹ
+                                năng 3 đang bật
+                              </div>
+                            </div>
+                            <Divider className="my-2" />
+                            <div className="flex items-center gap-4">
+                              <div className="w-20 rounded-md bg-orange-500 py-0.5 text-center text-sm text-white">
+                                Cam
+                              </div>
+                              <div className="flex-1">
+                                Tăng khả năng phòng thủ lên 24%; Tăng 100% hiệu quả của các pha nguy
+                                hiểm đã học (hiệu ứng kiểm soát được tăng lên bởi xác suất kích
+                                hoạt); Khi bạn có thẻ, bạn có thể chơi từ vòng đầu tiên
+                              </div>
+                            </div>
+                            <Divider className="my-2" />
+                            <div className="flex items-center gap-4">
+                              <div className="w-20 rounded-md bg-red-500 py-0.5 text-center text-sm text-white">
+                                Đỏ
+                              </div>
+                              <div className="flex-1">
+                                Tăng 30% sát thương kỹ năng; Chuyển đổi các pha nguy hiểm, tấn công
+                                các đơn vị kẻ thù để gây sát thương, đánh cắp một phần hiệu ứng của
+                                tất cả các pha nguy hiểm của kẻ thù và thêm chúng vào kỹ năng này
+                                (nếu bạn nhận được buff, hãy thêm nó vào người triệu hồi); Khi bạn
+                                lần đầu tiên phát hành hỗ trợ của mình, bạn có thể hồi sinh một
+                                chiến binh ngẫu nhiên về phía bạn một lần, khôi phục 30% sức khỏe
+                                của bạn và thừa hưởng sự tức giận; Số lần sử dụng +1
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                      <Divider className="my-4" />
-                      <div className="space-y-3">
-                        <h3 className="font-semibold">Đẳng viện trợ</h3>
-                        <div className="flex cursor-pointer flex-col gap-2 rounded-br-lg rounded-tl-lg border border-orange-400/25 px-2 py-4 drop-shadow">
-                          <div className="flex items-center gap-4">
-                            <div className="w-20 rounded-md bg-white py-0.5 text-center text-sm text-black">
-                              Trắng
-                            </div>
-                            <div className="flex-1">Kích hoạt kỹ năng viện trợ</div>
-                          </div>
-                          <Divider className="my-2" />
-                          <div className="flex items-center gap-4">
-                            <div className="w-20 rounded-md bg-emerald-500 py-0.5 text-center text-sm text-white">
-                              Lục
-                            </div>
-                            <div className="flex-1">
-                              Sau khi vào trận, bạn sẽ tăng vĩnh viễn tỷ lệ sát thương của bản thân
-                              thêm 15%, tỷ lệ tránh sát thương thêm 30% và tỷ lệ chặn đòn thêm 10%,
-                              khe kỹ năng số 1 được mở.
-                            </div>
-                          </div>
-                          <Divider className="my-2" />
-                          <div className="flex items-center gap-4">
-                            <div className="w-20 rounded-md bg-blue-500 py-0.5 text-center text-sm text-white">
-                              Lam
-                            </div>
-                            <div className="flex-1">
-                              Thêm 150% sát thương phòng thủ của chính nó, kỹ năng 2 được kích hoạt,
-                              +1 số lần sử dụng
-                            </div>
-                          </div>
-                          <Divider className="my-2" />
-                          <div className="flex items-center gap-4">
-                            <div className="w-20 rounded-md bg-purple-500 py-0.5 text-center text-sm text-white">
-                              Tím
-                            </div>
-                            <div className="flex-1">
-                              Phục hồi 12% máu tối đa cho người chiến đấu có lượng máu thấp nhất và
-                              thêm máu tương đương 200% khả năng phòng thủ của chính mình; Kỹ năng 3
-                              đang bật
-                            </div>
-                          </div>
-                          <Divider className="my-2" />
-                          <div className="flex items-center gap-4">
-                            <div className="w-20 rounded-md bg-orange-500 py-0.5 text-center text-sm text-white">
-                              Cam
-                            </div>
-                            <div className="flex-1">
-                              Tăng khả năng phòng thủ lên 24%; Tăng 100% hiệu quả của các pha nguy
-                              hiểm đã học (hiệu ứng kiểm soát được tăng lên bởi xác suất kích hoạt);
-                              Khi bạn có thẻ, bạn có thể chơi từ vòng đầu tiên
-                            </div>
-                          </div>
-                          <Divider className="my-2" />
-                          <div className="flex items-center gap-4">
-                            <div className="w-20 rounded-md bg-red-500 py-0.5 text-center text-sm text-white">
-                              Đỏ
-                            </div>
-                            <div className="flex-1">
-                              Tăng 30% sát thương kỹ năng; Chuyển đổi các pha nguy hiểm, tấn công
-                              các đơn vị kẻ thù để gây sát thương, đánh cắp một phần hiệu ứng của
-                              tất cả các pha nguy hiểm của kẻ thù và thêm chúng vào kỹ năng này (nếu
-                              bạn nhận được buff, hãy thêm nó vào người triệu hồi); Khi bạn lần đầu
-                              tiên phát hành hỗ trợ của mình, bạn có thể hồi sinh một chiến binh
-                              ngẫu nhiên về phía bạn một lần, khôi phục 30% sức khỏe của bạn và thừa
-                              hưởng sự tức giận; Số lần sử dụng +1
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </Tab>
+                    </Tab>
+                  )}
                   <Tab key="gallery" title="Hình ảnh">
                     <div className="sm:rounded-lg sm:border sm:border-slate-400/25 sm:p-4">
                       <div className="space-y-3">
